3a3a354134150eccc206d2381e1f8fae833587d2
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-14 17:13:42 +0900

    firebaseへデータを登録

---
+++ pages/todos.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="form">
+      <!-- submit時にaddメソッドを発火 preventでページのロードを無効にしている -->
+      <form action="" @submit.prevent="add">
+        <input type="text" name="" id="" v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // createdのタイミングでtodosの初期化
+  created(){
+    this.$store.dispatch('todos/init')
+  },
+
+  data() {
+    return {
+      name: "",
+      done: false
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+    // submitボタンをオスと呼ばれるメソッド
+    add(){
+      // storeのtodos.jsないのaddメソッドを実行(contexに入力したnameが入る)
+      this.$store.dispatch('todos/add', this.name)
+      // 処理が終わったら入力値を空にする
+      this.name = ""
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
